7a5bc66974f3171868284fc85d0a67c5cfcaa4f4
yangzhilinAndy <yy_2067@qq.com>
2020-06-16 23:50:26 +0800

    add zuul

---
+++ MyInstagramAPIZuulAPIGateway/MyInstagramAPIZuulAPIGateway/src/test/java/com/zhilin/myinstagram/api/gateway/MyInstagramApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhilin.myinstagram.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInstagramApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
